b66dfc12d4d279a6f47dc17adca3795e92760896
Miguel <miguellealw@gmail.com>
2022-04-26 04:05:05 -0500

    worked on view query

---
+++ view.sql
@@ -0,0 +1,44 @@
+
+/*
+	OrderDate
+	StartDate – in an ascending order
+	ReturnDate
+	Total Days per Rental as 'TotalDays'– you need to change weeks to days
+
+	Vehicle’s ID as 'VIN'
+	Vehicle’s Description as 'Vehicle'
+	Vehicle’s Type as 'Type'– you need to use the description of the type
+	Vehicle’s Category as 'Category' – you need to use the description of the category
+	Customer’s ID as 'CustomerID'
+	Customer’s Name as 'CustomerName'
+
+	Order Total Amount as 'OrderAmount',
+	Order Remaining Amount as 'RentalBalance' – If there is no remaining balance return zero ‘0’
+*/
+
+CREATE OR REPLACE VIEW vRentalInfo AS
+SELECT 
+	re.OrderDate, re.StartDate, re.ReturnDate, re.TotalAmount as "OrderAmount",
+	JULIANDAY(re.ReturnDate) - JULIANDAY(re.StartDate) as "TotalDays",
+	v.VehicleID as "VIN", v.Description as "Vehicle",
+	CASE 
+	    WHEN v.Type = 1 THEN "Compact"
+	    WHEN v.Type = 2 THEN "Medium"
+	    WHEN v.Type = 3 THEN "Large"
+	    WHEN v.Type = 4 THEN "SUV"
+	    WHEN v.Type = 5 THEN "Truck"
+	    WHEN v.Type = 6 THEN "Van"
+	    ELSE "Unsupported vehicle type"
+	END AS "Type",
+	CASE
+		WHEN v.Category = 0 THEN "Basic"
+		WHEN v.Category = 1 THEN "Luxury"
+		ELSE "Unsupported vehicle category"
+	END AS "Category", 
+	c.CustID as "CustomerID", c.Name as "CustomerName",
+	SUM(TotalAmount) as 'RentalBalance'
+FROM RENTAL re
+INNER JOIN CUSTOMER c ON re.CustID = c.CustID
+INNER JOIN VEHICLE v ON re.VehicleID = v.VehicleID
+GROUP BY re.CustID
+ORDER BY re.StartDate;
