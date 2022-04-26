a4472ae334b840eb50a98c1f7108aaddaf23950f
Miguel <miguellealw@gmail.com>
2022-04-26 02:58:20 -0500

    DB connection should now be easier to use

--- connect_to_db.py
+++ connect_to_db.py
@@ -2,31 +2,26 @@ import sqlite3
 from sql_startup import startup
 from os.path import exists
 
-def connect_to_db():
+def connect_to_db(db_name):
 	try:
-		cursor = None
 		conn = None
 
 		# only run startup script if the DB does not exist
-		if not exists('./car_rental.db'):
+		if not exists('./' + db_name):
 			# Connect to DB
-			conn = sqlite3.connect('car_rental.db')
+			conn = sqlite3.connect(db_name)
 			print("Connected to DB successfully")
 			cursor = conn.cursor()
 
 			cursor.executescript(startup)
 			print("Ran startup DB script successfully")
 		else:
-			conn = sqlite3.connect('car_rental.db')
+			conn = sqlite3.connect(db_name)
 			print("Connected to DB successfully")
 			cursor = conn.cursor()
 			print("DB already exists so the startup script did not run")
 
-
-		conn.close()
-
 	except Exception as ep:
 		print("Error connecting to DB", ep)
-		conn.close()
-	finally:
-		conn.close()
+	
+	return conn
