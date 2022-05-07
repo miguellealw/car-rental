bd3f3092a98b330bb31e1cc6594d5cede46e2d7a
Miguel <miguellealw@gmail.com>
2022-05-06 23:44:46 -0500

    updated view

--- sql_startup.py
+++ sql_startup.py
@@ -187,4 +187,37 @@ INSERT INTO VEHICLE ("VehicleID", "Description", "Year", "Type", "Category") VAL
 ('YV4940NB5F1191453', 'Volvo XC70', '2015', '4', '1');
 
 
+
+-- Create View
+CREATE VIEW vRentalInfo AS
+	SELECT 
+		c.CustID as "CustomerID", c.Name as "CustomerName",
+		v.VehicleID as "VIN", v.Description as "Vehicle",
+		re.OrderDate, re.StartDate, re.ReturnDate, re.TotalAmount as "OrderAmount",
+		JULIANDAY(re.ReturnDate) - JULIANDAY(re.StartDate) as "TotalDays",
+		
+		CASE 
+		    WHEN v.Type = 1 THEN "Compact"
+		    WHEN v.Type = 2 THEN "Medium"
+		    WHEN v.Type = 3 THEN "Large"
+		    WHEN v.Type = 4 THEN "SUV"
+		    WHEN v.Type = 5 THEN "Truck"
+		    WHEN v.Type = 6 THEN "Van"
+		    ELSE "Unsupported vehicle type"
+		END AS "Type",
+		
+		CASE
+			WHEN v.Category = 0 THEN "Basic"
+			WHEN v.Category = 1 THEN "Luxury"
+			ELSE "Unsupported vehicle category"
+		END AS "Category", 
+		
+		SUM(TotalAmount) as 'RentalBalance'
+		
+	FROM RENTAL re
+	INNER JOIN CUSTOMER c ON re.CustID = c.CustID
+	INNER JOIN VEHICLE v ON re.VehicleID = v.VehicleID
+	GROUP BY re.OrderDate, re.CustID, re.VehicleID
+	ORDER BY re.StartDate;
+
 """
